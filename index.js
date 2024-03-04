f2489665bdd8be50b70abb246f8e25a0fa3d5f9d
Yutamo <alca.amb.96@gmail.com>
2024-03-03 20:02:28 -0500

    ya se realiza envio de informacion por post y recepcion por get

--- index.js
+++ index.js
@@ -1,21 +1,45 @@
 const express = require("express");
-const app = express();
+//importamos CORS
+const cors = require("cors");
 
+//SE CREA LA APP
+const app = express();
+//se referencia el puerto
 const port = 3000;
 
-
+//se referencian los archivos static
 app.use(express.static('public'));
 //usa el modulo cors para que no hayan restricciones
 app.use(cors());
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+)
 //permite usar json en express
-app.use(express.json());
+app.use(express.json({
+  type:"*/*"
+}));
+
+//------------------------------------------**********************--------------------*********************************------------
+let registrosApp = [];
 
 
+//------------------------------------------**********************--------------------*********************************------------
 
+//cuando me hagan un GET al http://localhost:3000/registro
 app.get("/", (req,res) => {
-  res.send("HOLA MUNDO");
+  res.send(registrosApp);
 });
 
+//cuando me hagan un POST al http://localhost:3000/registro
+app.post("/registro", (req, res) =>{
+  const body = req.body;
+  registrosApp.push(body);
+  res.send(JSON.stringify("Peticion Guardada"));
+  console.log(registrosApp);
+
+});
 
 
 app.listen(port, () =>{
--- public/main.js
+++ public/main.js
@@ -1,1 +1,45 @@
-alert("hola");
+//ELEMENTOS DEL FORMULARIO
+const formularioRegistro = document.getElementById("formulario-registro");
+
+
+function iniciarApp() {
+  formularioRegistro.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const nombre = document.getElementById("nombre").value;
+    const correo = document.getElementById("correo").value;
+    const telefono = document.getElementById("telefono").value;
+    const empresa = document.getElementById("empresa").value;
+
+    let transaction = {
+      nombre: nombre,
+      correo: correo,
+      telefono: telefono,
+      empresa: empresa
+    }
+
+    let transactionJson = JSON.stringify(transaction);
+    console.log(transactionJson);
+
+    //mandar los datos al backend y guardalos ahi
+    fetch("http://localhost:3000/registro",{
+      method: "POST",
+      headers:{
+        "content-Type": "application/json"
+      },
+      body: transactionJson
+    })
+    .then(response => response.json())
+    .then(data => {
+      console.log(data);
+    })
+    .catch(error => {
+      console.log("Error al enviar la solaicitud", error);
+    })
+
+  })
+}
+
+
+window.addEventListener("load", iniciarApp);
+
